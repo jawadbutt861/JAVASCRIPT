3294371eb1a2e53d4a9a5457b78968c780cf75ab
Jawadbutt861 <jawadbutt861@gmail.com>
2025-05-24 16:35:10 +0500

    updated

---
+++ CONCAT/script.js
@@ -0,0 +1,27 @@
+// TASK 1
+
+var street = prompt("Enter Your Street");
+var city = prompt("Enter Your City");
+var country = prompt("Enter Your Country");
+console.log("Your full Address is:" + street + "," + city + "," + country);
+
+// TASK 2
+var Name = prompt("Enter Your Name");
+var number = prompt("Enter Your Favourite Number");
+console.log("Your New Username is " + Name + number);
+
+// TASK 3
+var Fname = prompt("Enter Your First Name");
+var Lname = prompt("Enter Your Last Name");
+console.log("Your E-mail is " + Fname + Lname + "@gmail.com");
+
+// TASK 4
+var color = prompt("Enter Your Favourite Color");
+var animal = prompt("Enter Your Favourite Animal");
+var food = prompt("Enter Your Favourite Food");
+console.log("Your Favourite Things are " + color + "," + animal + "," + food + "!");
+
+// TASK 5
+var quote = prompt("Enter a Quote");
+var author = prompt("Enter author Name");
+console.log(quote + " - " + author);
