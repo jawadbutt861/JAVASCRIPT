c4b57f0abd7df4157691fa252820559d30d16767
Jawadbutt861 <jawadbutt861@gmail.com>
2025-05-24 16:20:52 +0500

    updated

---
+++ ALERT,PROMPT,CONSOLE & CONCAT/script.js
@@ -0,0 +1,26 @@
+// TASK 1
+var Name = prompt("Enter Your Name");
+alert("Welcome, " + Name + "!");
+
+// TASK 2
+var Fname = prompt("Enter Your Name");
+var Age = prompt("Enter Your Age");
+console.log(Fname + " is " + Age + " years old");
+
+// TASK 3
+var color = prompt("Enter Your Favorite Color");
+alert("Wow! " + color + " is a nice color!");
+
+// TASK 4
+
+var num1 = prompt("Enter First Number");
+var num2 = prompt("Enter Second Number");
+var sum = num1 + num2;
+console.log("The Sum of "+ num1 + " + " + num2 + " is " + sum);
+
+// TASK 5
+
+var username = prompt("Enter Your Name");
+var hobby = prompt("Enter Your Hobby");
+var country = prompt("Enter Your Country");
+alert("Hi, I'm " + username + " from " + country + " and I love " + hobby + "!");
