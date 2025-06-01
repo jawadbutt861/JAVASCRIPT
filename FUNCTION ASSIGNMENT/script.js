03e09410aebaefd6d5e8df142e8bf453c89de108
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-01 20:31:22 +0500

    updated

---
+++ FUNCTION ASSIGNMENT/script.js
@@ -0,0 +1,170 @@
+// addTwoNumbers
+
+// function sum(num1,num2)
+// {
+//     console.log(num1 + num2);
+//     return num1 + num2;
+// }
+// var num1 = prompt("Enter value of num1");
+// var num2 = prompt("Enter value of num2");
+// console.log(sum(num1,num2));
+
+// subtractNumbers
+
+// function subtract(num1,num2)
+// {
+//     console.log(num1 - num2);
+//     return num1 - num2;
+// }
+// var num1 = prompt("Enter value of num1");
+// var num2 = prompt("Enter value of num2");
+// console.log(subtract(num1,num2));
+
+//Multiply
+
+// function product(num1,num2)
+// {
+//     console.log(num1 * num2);
+//     return num1 * num2;
+// }
+// var num1 = prompt("Enter value of num1");
+// var num2 = prompt("Enter value of num2");
+// console.log(product(num1,num2));
+
+//Divide
+
+// function divide(num1,num2)
+// {
+//     console.log(num1 / num2);
+//     return num1 / num2;
+// }
+// var num1 = prompt("Enter value of num1");
+// var num2 = prompt("Enter value of num2");
+// console.log(divide(num1,num2));
+
+// Reminder
+
+// function reminder(num1,num2)
+// {
+//     console.log(num1 % num2);
+//     return num1 % num2;
+// }
+// var num1 = prompt("Enter value of num1");
+// var num2 = prompt("Enter value of num2");
+// console.log(reminder(num1,num2));
+
+// Positive
+
+// function ispositive(num)
+// {
+//     if(num > 0)
+//     {
+//         return "Positive";
+//     }
+//     else
+//     {
+//         return "Not Positive";
+//     }
+// }
+// var num = prompt("Enter Number");
+// console.log(ispositive(num));
+
+
+// Negative
+
+// function isnegative(num)
+// {
+//     if(num < 0)
+//     {
+//         return "Negative";
+//     }
+//     else
+//     {
+//         return "Not Negative";
+//     }
+// }
+// var num = prompt("Enter Number");
+// console.log(isnegative(num));
+
+// Even Odd
+
+// function iseven(num)
+// {
+//     if(num %2 === 0)
+//     {
+//         return "Even";
+//     }
+//     else
+//     {
+//         return "Odd";
+//     }
+// }
+// var num = prompt("Enter Number");
+// console.log(iseven(num));
+
+// Equal
+
+// function isequal(num1,num2)
+// {
+//     if(num1 === num2)
+//     {
+//         return "Equal";
+//     }
+//     else
+//     {
+//         return "Not Equal";
+//     }
+// }
+
+// var num1 = prompt("Enter value of number 1");
+// var num2 = prompt("Enter value of number 2");
+// console.log(isequal(num1,num2));
+
+// Compare
+
+// function compare(num1,num2)
+// {
+//     if(num1 > num2)
+//     {
+//         return "Greater";
+//     }
+//     else
+//     {
+//         return "Smaller";
+//     }
+// }
+
+// var num1 = prompt("Enter value of number 1");
+// var num2 = prompt("Enter value of number 2");
+// console.log(compare(num1,num2));
+
+
+
+// Fullname
+
+// function fullname(fname,lname)
+// {
+//        return " fname " + " lname ";
+// }
+
+// var fname = prompt("Enter your first name");
+// var lname = prompt("Enter your last name");
+// console.log(compare(fname,lname));
+
+
+// marks
+
+function grade(marks)
+{
+    if(marks >= 40)
+    {
+        return "Pass";
+    }
+    else
+    {
+        return "Fail";
+    }
+}
+var marks = prompt("Enter your marks(0-100)");
+console.log(grade(marks));
+
