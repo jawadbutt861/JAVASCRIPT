f43864b589cd4ffd4633ad028ec3efecda0f82e1
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-01 20:06:17 +0500

    updated

--- FUNCTION/script.js
+++ FUNCTION/script.js
@@ -21,3 +21,18 @@ console.log("Sub is " + sub(num1,num2));
 console.log("Mul is " + mul(num1,num2));
 console.log("Div is " + div(num1,num2));
 
+function affidavit(name,fname,uni,start,department,semester,scholarship,cgpa){
+    return "It is certify that " + name + " son of " + fname + " is student of " + uni + " since " + start + ". He is doing bachelors in " + department +" and is currently enrolled in " + semester + " semester. He got " + scholarship + " Scholarships.His CGPA is " + cgpa + " He is brilliant student of our University."
+}
+var name = prompt("Enter Your Name");
+var fname = prompt("Enter Your Father Name");
+var uni = prompt("Enter Your University Name");
+var start = prompt("Enter Start Year of Degree");
+var department = prompt("Enter Your Department Name");
+var semester = prompt("Enter Your Current Semester");
+var scholarship = prompt("Enter Number of Scholarships You Got");
+var cgpa = prompt("Enter Your CGPA");
+
+console.log(affidavit(name,fname,uni,start,department,semester,scholarship,cgpa));
+
+
