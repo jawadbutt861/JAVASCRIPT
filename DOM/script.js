88f8bb73f7d55c2f9613cddddb3bf70e030da054
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-03 20:56:06 +0500

    updated

--- DOM/script.js
+++ DOM/script.js
@@ -30,3 +30,37 @@ if(subtitle.innerHTML === "old")
 
 var info = document.getElementById("info");
 console.log(info.innerHTML);
+
+var para = document.getElementById("para-1");
+para.innerHTML = "Welcome!";
+
+var note = document.getElementById("note");
+if(note.innerHTML === "")
+{
+    note.innerHTML = "No Notes found"
+}
+
+var status1 = document.querySelector("#status-1");
+
+if(status1.innerHTML === "Offline")
+{
+    status1.innerHTML = "Online";
+}
+
+var alert1 = document.querySelector(".alert-1");
+if(alert1.innerHTML === "Error")
+{
+    alert1.innerHTML = "Warning";
+}
+
+var counter = document.querySelector(".counter");
+if(counter.innerHTML > 10)
+{
+    counter.innerHTML = 0;
+}
+
+var usercontent = document.querySelector(".user-content");
+if(usercontent.innerHTML !== "admin")
+{
+    usercontent.innerHTML = "Guest";
+}
