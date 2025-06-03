64efa0ddc8095950b2d55e2911d90c8eba28d360
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-03 17:13:01 +0500

    updated

--- DOM/script.js
+++ DOM/script.js
@@ -6,4 +6,27 @@ function change()
     heading.innerHTML="I'm changed through JS " + num;
 }
 
+var stock = document.querySelector(".stock");
+function checkstock()
+{
+    if(stock.innerHTML === "Available")
+    {
+        stock.innerHTML = "Not available";
+    }
+    else
+    {
+        stock.innerHTML = "Available";
+    }
+}
 
+document.querySelector(".title").innerHTML="Hello World";
+    
+var subtitle = document.getElementById("subtitle");
+
+if(subtitle.innerHTML === "old")
+{
+    subtitle.innerHTML = "new";
+}
+
+var info = document.getElementById("info");
+console.log(info.innerHTML);
