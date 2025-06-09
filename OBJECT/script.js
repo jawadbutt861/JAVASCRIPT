dbdf76650c5d0c1b38114a42579f7b4b5adbf040
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-09 22:30:53 +0500

    UPDATED

---
+++ OBJECT/script.js
@@ -0,0 +1,44 @@
+const student = 
+{
+    Name : "Jawad",
+    Semester : "6th",
+    CGPA : 3.5
+};
+
+console.log(student.Semester);
+
+const product =
+{
+    productName : "Parket Jotter Standard CT Ball Pen",
+    color : "Black",
+    rating : 5.0,
+    price : "300$",
+    offer : true,
+    DiscountPercentage : 5
+};
+
+const profile =
+{
+    userName : "JawadButt",
+    isFollowed : false,
+    posts : 200,
+    followers : 3000,
+    following : 10,
+};
+
+console.log(profile.followers);
+console.log(profile);
+
+let car =
+{
+    carname : "Crown",
+    companyName : "Toyota",
+    color : "White",
+    model : 2024,
+    isAccidental : false,
+    condition : "Good",
+    price : 10000000
+};
+console.log(car);
+
+
--- VARIABLES AND DATA TYPES/script.js
+++ VARIABLES AND DATA TYPES/script.js
@@ -9,5 +9,15 @@ var age=18;
 console.log(false);
 
 
+let name = "Jawad";
+// let name = "Jawad"; it will gives an error
+name ="Ali";
+console.log(name);
+
+const isFollowed = false;
+// const isFollowed = false; also gives an error
+// isFollowed = true gives error in console
+
+
 
 
