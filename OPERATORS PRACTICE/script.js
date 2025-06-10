aca82786d43fa0bf57ffcb67701ccf11edadfeec
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-10 14:28:21 +0500

    updated

---
+++ OPERATORS PRACTICE/script.js
@@ -0,0 +1,59 @@
+// let a = 5;
+// let b = 2;
+
+// Arithemetic Operators
+
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b);
+
+// Unary Operators
+
+// console.log("++a = ", ++a);
+// console.log("a++ = ", a++);
+// console.log("a++ = ", a);
+
+// console.log("--b = ", --b);
+// console.log("a-- = ", a--);
+// console.log("a-- = ", a);
+
+// Assignment Operators
+
+// let c = 10;
+
+// c += 4;
+// console.log("c += 4 =",c);
+
+// c -= 4;
+// console.log("c -= 4 =",c);
+
+// c *= 4;
+// console.log("c *= 4 =",c);
+
+// c /= 4;
+// console.log("c /= 4 =",c);
+
+// c %= 4;
+// console.log("c %= 4 =",c);
+
+// c **= 4;
+// console.log("c **= 4 =",c);
+
+// Comparison Operators
+
+let a = 5;
+let b = 4;
+
+console.log("5 == 4", a == b);
+
+console.log("5 != 4", a != b);
+console.log("5 === 4", a === b);
+console.log("5 !== 4", a !== b);
+console.log("5 >= 4", a >= b);
+console.log("5 <= 4", a <= b);
+console.log("5 > 4", a > b);
+console.log("5 < 4", a < b);
+
