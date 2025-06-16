ffda7be77ed59ef4da72e41afa1d9d18bdbd789f
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-16 22:13:14 +0500

    updated

---
+++ ARRAY/script.js
@@ -0,0 +1,33 @@
+let fruits = ["Peach","Kiwi","Watermelon","Melon","Strawberry","Mango","Apple","Banana","Cherry","Papaya","Avacado"];
+for(let i of fruits)
+{
+    console.log(i);
+}
+
+let color = ["Green","Red","Blue","White","Yellow","Black"];
+console.log(color.push("Pink"));
+console.log(color);
+
+console.log(color.pop());
+console.log(color);
+
+console.log(color.unshift("Brown"));
+console.log(color);
+
+console.log(color.shift());
+console.log(color);
+
+console.log(color.slice(1,4));
+console.log(color.splice(1,1,"Crimson"));
+console.log(color);
+
+console.log(color.includes("Blue"));
+
+
+
+
+
+
+
+
+
--- STRINGS PRACTICE/script.js
+++ STRINGS PRACTICE/script.js
@@ -1,4 +1,37 @@
-let username =  prompt("Enter Your Full Name(without spaces)");
+// let username =  prompt("Enter Your Full Name(without spaces)");
 
-let str = "@";
-console.log(`Your Username is ${str + username + username.length}`);
+// let str = "@";
+// console.log(`Your Username is ${str + username + username.length}`);
+
+
+let str2 = "I love JS";
+console.log(str2.charAt(5));
+console.log(str2.indexOf("JS"));
+console.log(str2.slice(2));
+console.log(str2.replace("JS","PYTHON"));
+console.log(str2.toLowerCase());
+console.log(str2.toUpperCase());
+
+for(let i of str2)
+{
+    console.log(i);
+    
+}
+
+let firstName = "Jawad";
+let lastName = "Safdar";
+console.log(firstName.concat(lastName));
+
+
+let num1 = 10;
+let num2 = 20;
+console.log(`The Sum of ${num1} and ${num2} is ${num1 + num2}`);
+console.log(`The Subtraction of ${num1} and ${num2} is ${num1 - num2}`);
+console.log(`The Product of ${num1} and ${num2} is ${num1 * num2}`);
+console.log(`The Division of ${num1} and ${num2} is ${num1 / num2}`);
+
+
+
+
+
+
