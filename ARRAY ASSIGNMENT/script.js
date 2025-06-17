ddecea52988b6f5bea61f70c8be0c7770ab5c648
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-17 23:14:46 +0500

    updated

---
+++ ARRAY ASSIGNMENT/script.js
@@ -0,0 +1,94 @@
+// Question 1
+
+let names = ["Jawad","Ali","Ahmed"];
+names.push("Salaar");
+console.log(names);
+
+// Question 2
+
+let fruits = ["Apple","Mango","Banana","Kiwi","Grapes","Peach","Cherry","Strawberry"];
+fruits.pop();
+console.log(fruits);
+
+// Question 3
+
+let days = ["Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+days.unshift("Monday");
+console.log(days);
+
+// Question 4
+
+let colors = ["Green","Yellow","Blue","White","Black","Red","Purple"];
+colors.shift();
+console.log(colors);
+
+// Question 5
+
+let subjects = ["English","Urdu","Islamyat","Physics","Computer","Chemistry"];
+subjects.splice(2,0,"Math","Science");
+console.log(subjects);
+
+// Question 6
+
+let cities = ["Quetta","Rawalpindi","Islamabad","Lahore","Karachi","Peshawar"];
+cities.splice(1,2);
+console.log(cities);
+
+// Question 7
+
+let countries = ["Pakistan","China","Qatar","Dubai","Iran","Kuwait","Behrain"];
+console.log(countries.slice(0,3));
+
+// Question 8
+
+let str = "Apple,Banana,mango";
+console.log(str.split(" "));
+
+// Question 9
+// Question 10
+
+let array1 = ["Pen","Pencil"];
+let array2 = ["Eraser","Sharpener"];
+console.log(array1.concat(array2));
+
+// Question 11
+
+
+// Question 12
+// Question 13
+
+let nums = [1,2,3,4,5,6,7,8,9,10];
+console.log(nums.reverse());
+
+// Question 14
+
+let colors2 = ["Green","Yellow","Blue","White","Black","Red","Purple"];
+console.log(colors2.indexOf("Blue"));
+
+// Question 15
+
+let empty = [];
+empty.push("empty1","empty2","empty3");
+console.log(empty);
+
+// Question 16
+
+let nums2 = [1,2,3,4,5];
+nums2.splice(2,1);
+console.log(nums2);
+
+// Question 17
+// Question 18
+// Question 19
+
+let cities2 = ["Quetta","Rawalpindi","Islamabad","Lahore","Karachi","Peshawar"];
+console.log(cities2.indexOf("Karachi"));
+
+// Question 20
+
+let even = [2,4,6,8,10];
+let odd = [1,3,5,7,9];
+console.log(even.concat(odd).reverse());
+
+
+
--- ARRAY/script.js
+++ ARRAY/script.js
@@ -1,27 +1,71 @@
-let fruits = ["Peach","Kiwi","Watermelon","Melon","Strawberry","Mango","Apple","Banana","Cherry","Papaya","Avacado"];
-for(let i of fruits)
-{
-    console.log(i);
-}
+// // let fruits = ["Peach","Kiwi","Watermelon","Melon","Strawberry","Mango","Apple","Banana","Cherry","Papaya","Avacado"];
+// // for(let i of fruits)
+// // {
+// //     console.log(i);
+// // }
 
-let color = ["Green","Red","Blue","White","Yellow","Black"];
-console.log(color.push("Pink"));
-console.log(color);
+// // let color = ["Green","Red","Blue","White","Yellow","Black"];
+// // console.log(color.push("Pink"));
+// // console.log(color);
 
-console.log(color.pop());
-console.log(color);
+// // console.log(color.pop());
+// // console.log(color);
 
-console.log(color.unshift("Brown"));
-console.log(color);
+// // console.log(color.unshift("Brown"));
+// // console.log(color);
 
-console.log(color.shift());
-console.log(color);
+// // console.log(color.shift());
+// // console.log(color);
 
-console.log(color.slice(1,4));
-console.log(color.splice(1,1,"Crimson"));
-console.log(color);
+// // console.log(color.slice(1,4));
+// // console.log(color.splice(1,1,"Crimson"));
+// // console.log(color);
 
-console.log(color.includes("Blue"));
+// // console.log(color.includes("Blue"));
+
+// // let even = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30];
+
+// // console.log(even[5]);
+
+// // let odd = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29];
+// // console.log(odd);
+
+// let marks = [85,97,44,37,76,60]
+// let total = 0;
+// for(let i of marks)
+// {
+//     total += i;
+// }
+// let avg = total/marks.length;
+
+// console.log(`Average is ${avg}`);
+
+
+// let prices = [250,645,300,900,50];
+// let index = 0;
+// for(let i of prices)
+// {
+//    let offer = i/10;
+//    prices[index] = prices[index] - offer;
+//    console.log(`The discounted price is ${prices[index]}`);
+//    index++;
+// }
+
+let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+
+companies.shift();
+console.log(companies);
+
+companies.splice(1,1,"Ola");
+console.log(companies);
+
+companies.push("Amazon");
+console.log(companies);
+
+
+
+
+
 
 
 
