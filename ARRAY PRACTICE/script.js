d66ae89ae44b27af2519fd81284e546815ca1df5
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-20 19:07:21 +0500

    updated

---
+++ ARRAY PRACTICE/script.js
@@ -0,0 +1,20 @@
+let fruits = ["Apple","Mango","Peach","Banana","Watermelon"];
+console.log(fruits[2]);
+fruits[5] = "Melon";
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
+console.log(fruits.length*2);
+
+console.log(fruits.splice(1,1,"Kiwi"));
+
+let str = "abc";
+console.log(str.split(""));
+
+let array1 = [1,2,3,4,5];
+let array2 = [6,7,8,9,10];
+console.log(array1.concat(array2));
+console.log(array1.slice(0,3));
+
+
+
