7928c0ccd5ccf4d295044278ab2fb553d97def51
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-25 00:16:59 +0500

    updated

--- FUNCTION PRACTICE/script.js
+++ FUNCTION PRACTICE/script.js
@@ -87,42 +87,155 @@
 
 // console.log(mul(2,2));
 
-let count = 0;
-let vowels = (str) =>
-{
-    for(let i of str)
-    {
-        if(i == "a" || i == "e" || i == "i" || i == "o" || i == "u")
-        {
-            count++;
-        }
-    }
-}
+// let count = 0;
+// let vowels = (str) =>
+// {
+//     for(let i of str)
+//     {
+//         if(i == "a" || i == "e" || i == "i" || i == "o" || i == "u")
+//         {
+//             count++;
+//         }
+//     }
+// }
 
-let str = prompt("Enter a String");
-vowels(str);
-console.log(count);
-
-
-
-let sub = (a,b) =>
-{
-    return "Subtraction is ",a-b;
-}
-sub(5,2);
+// let str = prompt("Enter a String");
+// vowels(str);
+// console.log(count);
 
 
 
-function sum(a,b)
-{
-    return a + b
-}
+// let sub = (a,b) =>
+// {
+//     return "Subtraction is ",a-b;
+// }
+// sub(5,2);
 
-console.log(sum(2,2));
 
 
-function greetUser(name,age,email){
-    return "Your Name is " + name + " and you are " + age + " years old and your email is " + email;
-}
+// function sum(a,b)
+// {
+//     return a + b
+// }
 
-console.log(greetUser("Jawad",22,"jawadbutt861@gmail.com"));
+// console.log(sum(2,2));
+
+
+// function greetUser(name,age,email){
+//     return "Your Name is " + name + " and you are " + age + " years old and your email is " + email;
+// }
+
+// console.log(greetUser("Jawad",22,"jawadbutt861@gmail.com"));
+
+
+// function output(val1,val2)
+// {
+//     if(val1 === "string" && val2 === "string")
+//     {
+//         return "Concate = " + val1 + val2;
+//     }
+//     else
+//     {
+//         return "Sum = ", val1 + val2;
+//     }
+
+// }
+
+// console.log(output("jawad","safdar"));
+
+
+// let cube = (num) =>
+// {
+//     return ["Square",num ** 2 ,"Cube",num ** 3];
+// }
+// let num = 3;
+// console.log(cube(num));
+
+// let message = (msg) =>
+// {
+//     if(msg == "undefined")
+//     {
+//         return "No Message";
+//     }
+//     else
+//     {
+//         return msg;
+//     }
+// }
+// let msg = "Hello";
+// console.log(message(msg));
+
+// let recursion = (num) =>
+// {
+//     if(num == 0)
+//     {
+//         return 0;
+//     }
+//     else
+//     {
+//         return num + recursion(num - 1);
+//     }
+// }
+// let num1 = 5;
+// console.log(recursion(num1));
+
+
+// let arr = [2,3,4,5,6,7,8,9,10]
+// arr.forEach(arr =>{
+//     console.log(arr ** 2);
+    
+// })
+
+// let fact = (num) =>
+// {
+//     if(num == 0)
+//     {
+//         return "Number is zero or less";
+//     }
+//     else
+//     {
+//         return num * fact(num - 1);
+//     }
+// }
+
+// console.log(fact(4));
+
+
+// let marks = [100,20,50,90,99,96,55,67,89,45,66,90,92,92,96,94,20,40,88,85,70];
+// let topper = marks.filter((val) =>{
+//     return val >= 90;
+// })
+// console.log(topper);
+
+// let n = prompt("Enter a number");
+// let arr = [];
+// for(i = 1; i <= n; i++)
+// {
+//     arr[i-1] = i;
+// }
+
+// let ans = arr.reduce((pre,next) =>{
+//     return pre + next;
+// })
+
+// console.log(ans);
+
+
+// let n = prompt("Enter a number");
+// let arr = [];
+// for(i = 1; i <= n; i++)
+// {
+//     arr[i-1] = i;
+// }
+
+// let ans = arr.reduce((pre,next) =>{
+//     return pre * next;
+// })
+
+// console.log(ans);
+
+
+// map returns a new array 
+// reduce the array to single value
+// flter the value on basis of condition
+// forEach returns changes in original array
