931697f659ab137a0e7e742bf798f512b3ede6c3
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-26 00:42:51 +0500

    updared

---
+++ DOM PRACTICE/script.js
@@ -0,0 +1,21 @@
+// let str = "from Apna College Students";
+// let h2 = document.querySelector("h2");
+// h2.innerText = h2.innerText + str;
+
+// let divs = document.querySelectorAll(".box");
+
+// let index = 1;
+// for(let div of divs)
+// {
+//     div.innerText = `Unique Text ${index}`;
+//     index++;
+// }
+
+document.body.style.padding = "0px";
+document.body.style.margin = "0px";
+let heading = document.querySelector("#heading");
+heading.style.color = "red";
+heading.style.backgroundColor = "black";
+heading.style.padding = "10px";
+heading.style.textAlign = "center";
+heading.style.fontSize = "40px";
