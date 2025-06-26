e5efbb3ca929a2ba494e32814421aad443490559
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-27 00:52:46 +0500

    updated

--- DOM PRACTICE/script.js
+++ DOM PRACTICE/script.js
@@ -11,11 +11,36 @@
 //     index++;
 // }
 
-document.body.style.padding = "0px";
-document.body.style.margin = "0px";
-let heading = document.querySelector("#heading");
-heading.style.color = "red";
-heading.style.backgroundColor = "black";
-heading.style.padding = "10px";
-heading.style.textAlign = "center";
-heading.style.fontSize = "40px";
+// document.body.style.padding = "0px";
+// document.body.style.margin = "0px";
+// let heading = document.querySelector("#heading");
+// heading.style.color = "red";
+// heading.style.backgroundColor = "black";
+// heading.style.padding = "10px";
+// heading.style.textAlign = "center";
+// heading.style.fontSize = "40px";
+
+// let div = document.querySelector(".class");
+// let out = div.getAttribute("class");
+// console.log(out);
+
+// let style = document.querySelector(".class");
+// console.log(style.getAttribute("style"));
+// style.setAttribute("style", "background-color : blue;")
+// console.log(style.getAttribute("style"));
+
+// let input = document.querySelector("input");
+// input.style.padding = "20px";
+// input.style.width = "300px";
+// input.style.borderRadius = "20px";
+// input.style.border = "1px solid black"
+
+let btn = document.createElement("button");
+btn.innerText = "Click me!";
+btn.style.backgroundColor = "red";
+btn.style.color = "white";
+document.querySelector("body").prepend(btn);
+
+let para = document.querySelector(".para");
+para.classList.add("content");
+console.log(para.classList);
