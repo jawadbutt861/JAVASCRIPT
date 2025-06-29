e504a76b9bd9b3ebcedb94795172c785a8650b38
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-30 00:03:52 +0500

    updated

---
+++ EVENTS PRACTICE/script.js
@@ -0,0 +1,37 @@
+// let body = document.querySelector("body");
+// let btn = document.querySelector("#mybtn");
+
+// btn.onclick = () =>
+// {
+//     body.style.backgroundColor = "red";
+// }
+
+// let btn = document.querySelector(".mybtn");
+// btn.addEventListener("click",() =>{
+//     alert(`My Name is JAWAD`);
+// })
+
+// let info = document.querySelector("#info");
+// let btn = document.querySelector(".hide");
+
+// btn.addEventListener("click",() =>{
+//     info.style.display = "none";
+// })
+
+// let div = document.querySelector(".div");
+// let btn = document.querySelector(".hide");
+
+// btn.addEventListener("click",() =>{
+//     div.innerHTML = "Changed by clicking Button";
+// })
+
+// let btn = document.querySelector(".hide");
+// btn.addEventListener("click",() =>{
+//     btn.disabled = true;
+// })
+
+let list = document.querySelector("#list");
+let li = document.createElement("li");
+li.innerText = "Yellow";
+list.append(li);
+
---
+++ EVENTS/script.js
@@ -0,0 +1,42 @@
+// let bg = document.querySelector(".bg");
+// let btn = document.querySelector(".btn");
+
+// let changeBg = () =>
+// {
+//     bg.style.color = "blue";
+//     bg.style.backgroundColor = "black";
+//     bg.style.padding = "20px";
+//     bg.style.fontSize = "30px";
+//     btn.style.padding = "10px";
+//     btn.style.fontSize = "20px";
+//     btn.style.backgroundColor = "blue";
+//     btn.style.border = "1px solid black";
+//     btn.style.margin = "20px"
+// }
+
+// let para = document.querySelector(".para");
+// para.onmouseover = (e) =>
+// {
+//     console.log(e);
+    
+//     para.style.color = "red";
+//     para.style.backgroundColor = "blue";
+//     para.style.padding = "20px";
+// }
+
+// let body = document.querySelector("body");
+// let btn = document.querySelector(".mode");
+
+// btn.addEventListener("click",()=>
+// {
+//     if(btn.innerHTML == "Dark Mode")
+//     {
+//         body.style.backgroundColor = "black";
+//         btn.innerHTML = "Light Mode";
+//     }
+//     else
+//     {
+//         body.style.backgroundColor = "white";
+//         btn.innerHTML = "Dark Mode";
+//     }
+// })
