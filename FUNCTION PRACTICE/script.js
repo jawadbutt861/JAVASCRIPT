3646b4e93da360b4dcdf1227625f059da70d47cb
Jawadbutt861 <jawadbutt861@gmail.com>
2025-07-02 02:28:22 +0500

    completed

--- FUNCTION PRACTICE/script.js
+++ FUNCTION PRACTICE/script.js
@@ -266,3 +266,10 @@
 // })
 
 // console.log(ans);
+
+function sum(num1,num2)
+{
+    return num1 + num2;
+}
+
+console.log(sum(2,3));
--- LOOP ASSIGNMENT/script.js
+++ LOOP ASSIGNMENT/script.js
@@ -128,18 +128,21 @@
 //     list.innerHTML += `${i},`;
 // }
 
-//20 incomplete
 
 // let arr = [5, 10, 15, 20];
 
-// let arr2 = [];
-// for(let i = 1; i <= 1; i++)
+// for(let j = 10; j >= 1; j--)
 // {
-//     for(let j = 10; j >= 1; j--)
-//     {
-//         arr.concat(j);
-//     }
+//     arr.push(j)
 // }
+// console.log(arr);
+// let sum = 0;
+// for(let i of arr)
+// {
+//     sum += i
+// }
+// console.log(sum);
+
 
 
 // console.log(arr2);
@@ -230,8 +233,151 @@
 // console.log(arr3);
 
 //29 not complete
-//30 not complete
-//31 not complete
+// let arr = [10, 99, 23, 87, 56];
+// for(let i = 0; i < arr.length; i++)
+// {
+//     if(arr[i] > arr[i + 1] || arr[i] < arr)
+// }
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let reverse = [];
+// for(let i = arr.length - 1; i >= 0; i--)
+// {
+//     reverse.push(arr[i])
+// }
+// console.log(reverse);
+
+// for(let i = 2; i <= 50; i++)
+// {
+//     let count = 0;
+//     for(let j = 2; j <= 50; j++)
+//     {
+//         if(i % j == 0)
+//         {
+//             count++;
+//         }
+//     }
+
+//     if(count == 1)
+//     {
+//         console.log(i);
+//     }
+
+// }
+
+// let arr = [11, 4, 99, 2, 47];
+// let smallest = 0;
+// let largest = 0;
+// for(let i = 0; i < arr.length; i++)
+// {
+//     if(arr[i] > arr [i+1]){
+//         largest = arr[i];
+//     }
+//     if(arr[i] < arr[i+1])
+//     {
+//         smallest = arr[i]; 
+//     }
+// }
+
+// let difference = largest - smallest;
+// console.log(difference);
+
+// let arr = [0, -1, -5, 6, 8, -3];
+// let count = 0;
+// for(let i of arr)
+// {
+//     if(i < 0)
+//     {
+//         count++;
+//     }
+// }
+
+
+// let arr = [];
+// for(let i = 0; i <= 50; i++)
+// {
+
+//     if(!(i %2 == 0))
+//     {
+//         arr[i]=i   
+//     }
+// }
+// console.log(arr);
+
+
+// let arr = [0, 1, 0, 3, 0, 5];
+// let num = 2
+// for(let i = 0; i < arr.length; i++)
+// {
+//     if(arr[i] == 0)
+//     {
+//         arr[i] += num;
+//         num += 4;
+//     }
+// }
+// console.log(arr);
+
+// let arr = [1, 2, 3, 4, 5, 6];
+// let swap = 3;
+// for(let  i = 0; i < arr.length; i++)
+// {
+//     if(i == 0 || i == arr.length - 1)
+//     {
+//         arr[i] = swap;
+//         swap += 4;
+//     }
+// }
+// console.log(arr);
+
+// let arr = [2, 4, 6, 8];
+// for(let i of arr)
+// {
+
+//     if(i %2 == 0)
+//     {
+//         continue;
+//     }
+//     else{
+//         console.log("Not all Numbers are even");
+//         break;
+//     }
+// }
+
+// let arr = [5, 10, 15, 20, 25];
+// let sum = 0;
+// let avg = sum / arr.length;
+// for(let i  of arr)
+// {
+//     sum += i;
+// }
+// for(let i = 0 ; i < arr.length; i++)
+// {
+//     if(sum / arr.length > i)
+//     {
+//         console.log(arr[i]);
+//     }
+// }
+
+
+// let arr = [2, 2, 2, 2, 2];
+// let equal;
+// for(let  i = 0; i < arr.length; i++)
+// {
+//     if(arr[i] == arr[0])
+//     {
+//         equal = true;
+//     }
+//     else
+//     {
+//         equal = false;
+//     }
+
+// }
+// console.log(equal);
+
+
+
 
 
 
