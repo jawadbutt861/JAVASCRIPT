b6ded10486541d777ce15bae129a3a843349e872
Jawadbutt861 <jawadbutt861@gmail.com>
2025-07-01 21:44:31 +0500

    updated

--- LOOP ASSIGNMENT/script.js
+++ LOOP ASSIGNMENT/script.js
@@ -57,11 +57,183 @@
 //     }
 // }
 
-let para = document.querySelector("#max")
-let arr = [45,22,89,100];
-let greater = arr.reduce((pre,next) =>{
+// let para = document.querySelector("#max")
+// let arr = [45,22,89,100];
+// let greater = arr.reduce((pre,next) =>{
     
-    return pre > next ? pre : next;
-})
+//     return pre > next ? pre : next;
+// })
 
-para.innerHTML = greater;
+// para.innerHTML = greater;
+
+
+// let res = document.querySelector("#result");
+// res.innerHTML = "Task Complete";
+
+// let temperatures = [30, 35, 32, 40];
+// let averageTemp = document.querySelector("#averageTemp");
+// let avg = 0;
+// for(let i of temperatures)
+// {
+//     avg += i;
+// }
+// averageTemp.innerHTML = avg;
+
+// let totalSum = document.querySelector("#totalSum");
+// let sum = 0;
+// for(let i = 1; i <= 100; i++)
+// {
+//     sum += i;
+// }
+// totalSum.innerHTML = sum;
+
+// let array = ["dog", "cat", "bird"];
+// let animals = document.querySelector("#animals");
+// for(let i of array)
+// {
+//     animals.innerHTML += `${i} ,`;
+// }
+
+// let title = document.querySelector("#title");
+// title.innerHTML = "Welcome to Javascript";
+
+// for(let i = 1; i <= 50; i++)
+// {
+//     if(i %5 == 0)
+//     {
+//         console.log(i);
+//     }
+// }
+
+
+// let scores = [80, 60, 90, 70];
+// let highscores = document.querySelector("#highscore");
+
+// for(let i of scores)
+// {
+//     if(i > 75)
+//     {
+//         highscores.innerHTML += `${i},`;
+//     }
+// }
+
+// let msg = document.querySelector("#message");
+// msg.innerHTML = "Updated Successfully";
+
+
+// let subjects = ["Math", "Science", "English"];
+// let list = document.querySelector("#subjectList");
+// for(let i of subjects)
+// {
+//     list.innerHTML += `${i},`;
+// }
+
+//20 incomplete
+
+// let arr = [5, 10, 15, 20];
+
+// let arr2 = [];
+// for(let i = 1; i <= 1; i++)
+// {
+//     for(let j = 10; j >= 1; j--)
+//     {
+//         arr.concat(j);
+//     }
+// }
+
+
+// console.log(arr2);
+
+// let nums = [45, 67, 12, 89, 34];
+
+// let greater = 0;
+// for(let i = 0; i < nums.length; i++)
+// {
+//     if( nums[i] > nums[i + 1])
+//     {
+//         greater = nums[i];
+//     }
+// }
+
+// console.log(greater);
+
+
+// let arr = [3, 8, 6, 1, 9, 2];
+// let count = 0;
+// for(let i of arr)
+// {
+//     if(i %2 == 0)
+//     {
+//         count++;
+//     }
+// }
+// console.log(count);
+
+
+// let arr = [20, 30, 40, 50];
+// let sum = 0;
+// for(let i of arr)
+// {
+//     sum += i;
+// }
+// console.log(sum/arr.length);
+
+// let arr = [2, 4, 6, 8, 10];
+// let arr2 = [];
+// for(let i = 0; i < arr.length; i++)
+// {
+//     arr2[i] = arr[i]*3;
+// }
+// console.log(arr2);
+
+
+// let arr = [1, 3, 5, 7, 9];
+// let count = 0;
+// for(let i of arr)
+// {
+//     if(i > 4)
+//     {
+//         count++;   
+//     }
+// }
+// console.log(count);
+
+// let arr = [10, 15, 20, 25, 30];
+// let sum = 0;
+// for(let i of arr)
+// {
+//     if(i %5 == 0)
+//     {
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+// let arr = [10, 20, 30, 40, 25];
+// for(let i of arr)
+// {
+//     if( i == 25)
+//     {
+//         console.log("exist");
+        
+//     }
+// }
+
+
+// let arr1 = [1,2,3];
+// let arr2 = [4,5,6];
+// let arr3 = [];
+// for(let i = 0; i < arr1.length; i++)
+// {
+//     arr3[i] = arr1[i] + arr2[i];
+// }
+// console.log(arr3);
+
+//29 not complete
+//30 not complete
+//31 not complete
+
+
+
+
+
