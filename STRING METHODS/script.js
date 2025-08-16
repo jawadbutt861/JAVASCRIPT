050611840902b1acda0a9d17a2b68c14b9e67166
Jawadbutt861 <jawadbutt861@gmail.com>
2025-08-16 21:26:52 +0500

    updated

---
+++ STRING METHODS/script.js
@@ -0,0 +1,8 @@
+let str = "Hello Coders";
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.slice(6));
+console.log(str.indexOf("d"));
+console.log(str.split("").reverse().join(""));
+
