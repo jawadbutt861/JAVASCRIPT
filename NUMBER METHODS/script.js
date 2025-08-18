42db1864812882aa911aaf5665f1304f8834569e
Jawadbutt861 <jawadbutt861@gmail.com>
2025-08-18 22:28:33 +0500

    updated

---
+++ NUMBER METHODS/script.js
@@ -0,0 +1,17 @@
+// let num = 25.54
+// console.log(Math.round(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+// console.log(Math.random(num));
+// console.log(Math.trunc(num));
+// console.log(Math.sign(num));
+// console.log(Math.pow(4,3));
+// console.log(Math.max(25,12,-20,100,-100,45));
+// console.log(Math.min(25,12,-20,100,-100,45));
+// console.log(Math.sqrt(64));
+
+
+
+
+
+
