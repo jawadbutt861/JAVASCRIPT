13bb6cbcb081f9079b531f919ba6ccc61d7d336b
Jawadbutt861 <jawadbutt861@gmail.com>
2025-08-20 14:47:24 +0500

    updated

---
+++ COMPLETE PRACTICE/script.js
@@ -0,0 +1,111 @@
+// // Console
+
+// let a = 20;
+// console.log(a);
+
+// // Alert
+
+// let name = "JAWAD BUTT";
+// alert(name);
+
+// // Arithmetic Operators
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+// // Assignment Operators
+
+// let age = 22;
+// console.log(age += 2);
+// console.log(age -= 2);
+// console.log(age *= 2);
+// console.log(age /= 2);
+// console.log(age %= 2);
+
+// // Comparison Operators
+
+// let num1 = 20;
+// let num2 = 10;
+// console.log(num1 > num2);
+// console.log(num1 >= num2);
+// console.log(num1 < num2);
+// console.log(num1 <= num2);
+// console.log(num1 != num2);
+// console.log(num1 !== num2);
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+
+// // Logical Operators
+
+// console.log(num1 > num2 && num1 === num2);
+// console.log(num1 > num2 || num1 === num2);
+// console.log(!(num1 > num2));
+
+// Object
+
+// let student = {
+//     Name : "Jawad",
+//     Age : 22,
+//     CMS_ID : 62024,
+//     Semester : "7th",
+//     Repeat : false,
+//     BEEF : true,
+//     Merit : true,
+//     BEEF_NO : 2,
+//     Merit_NO : 1,
+//     Skills : true,
+// };
+// let arr = [];
+// console.log(arr.push(student));
+// console.log(arr);
+
+
+
+// console.log(student);
+// console.log(student.Merit);
+// console.log(student.BEEF_NO);
+// console.log(student.Name);
+
+// Array
+
+// let colors = ["Red","Yellow","Green","White","Pink","Blue"];
+// console.log(colors);
+// for(let i = 0; i <= colors.length; i++)
+// {
+//     console.log(colors[i]);
+// }
+// // for(let i of colors)
+// // {
+// //     console.log(i);
+// // }
+
+// // Loops
+
+// for(let i = 0; i <= 10; i++)
+// {
+//     console.log(i);
+// }
+
+// let j = 0;
+// while(j <= 10)
+// {
+//     console.log(j);
+//     j++;   
+// }
+
+// let k = 0;
+// do
+// {
+//     console.log(k);
+//     k++;   
+// }while(k <= 10);
+
+
+// 
+
+
+
+
