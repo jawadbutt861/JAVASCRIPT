f4af555b03e948689d437dfa32194369d0b43022
Jawadbutt861 <jawadbutt861@gmail.com>
2025-08-31 21:17:40 +0500

    updated

---
+++ DATE & TIME/script.js
@@ -0,0 +1,10 @@
+let currentDate = new Date();
+console.log(currentDate.getDate());
+console.log(currentDate.getDay());
+console.log(currentDate.getHours());
+console.log(currentDate.getMinutes());
+console.log(currentDate.getSeconds());
+console.log(currentDate.getMilliseconds());
+console.log(currentDate.getMonth());
+console.log(currentDate.getTime());
+console.log(currentDate.getFullYear());
