ec281cb4177e4cb507e600f9ca48498a58a4fcdd
Jawadbutt861 <jawadbutt861@gmail.com>
2025-09-09 02:28:06 +0500

    updated

--- DATE & TIME ASSIGNMENT/script.js
+++ DATE & TIME ASSIGNMENT/script.js
@@ -1,5 +1,48 @@
-let currentDate = new Date();
+// let currentDate = new Date();
 
-function Data(){
+// function Data(date){
+
+//     let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+//     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+//     let day = date.getDay();
+
+//     let month = date.getMonth()
+
+//     return `${days[day]} , ${date.getDate()} ${months[month]} ${date.getFullYear()}`
+// }
+
+
+// console.log(Data(currentDate));
+
+// let time1 = new Date("2025-01-12 12:00:00 AM");
+// let time2 = new Date("2025-09-09 12:00:00 AM");
+
+// console.log(`Minutes Passed : ${(time2 - time1) / 1000 / 60}`);
+
+
+// let currentDate = new Date();
+
+
+// function DoB(year,month,day){
+//     let dob = new Date(year,month,day);
+
+   
     
-}
+//     let years = currentDate.getFullYear() - dob.getFullYear();
+//     let months = currentDate.getMonth() - dob.getMonth();
+//     let days = currentDate.getDate() - dob.getDate();
+//     return `${years} - ${months} - ${days}`
+// }
+
+// console.log(DoB(2003,1,12));
+
+// let today = new Date();
+
+// function getDay(){
+//     let first = new Date(today.getFullYear(),today.getMonth(),1)
+//     return first;
+// }
+
+// console.log(getDay());
