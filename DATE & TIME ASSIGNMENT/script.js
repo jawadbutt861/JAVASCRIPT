ab7639dfa98fa53b97e26d88b57d1b889f444d30
Jawadbutt861 <jawadbutt861@gmail.com>
2025-09-17 23:09:10 +0500

    updated

--- DATE & TIME ASSIGNMENT/script.js
+++ DATE & TIME ASSIGNMENT/script.js
@@ -48,13 +48,69 @@
 // console.log(getDay());
 
 
-function lastDay(){
-    let current = new Date();
-    let year = current.getFullYear();
-    let month= current.getMonth();
+// function lastDay(){
+//     let current = new Date();
+//     let year = current.getFullYear();
+//     let month= current.getMonth();
 
-    let lastDay = new Date(year,month + 1 , 0).getDate();
-    return lastDay;
-}
+//     let lastDay = new Date(year,month + 1 , 0).getDate();
+//     return lastDay;
+// }
 
-console.log(lastDay());
+// console.log(lastDay());
+
+
+// function timeAgo() {
+//   let time = Date.now() - (55 * 60 * 1000); // 2 minutes ago
+//   let time2 = (Date.now() - time) / 1000;
+//   if (time2 < 60) return "Just now";
+//   if (time2 < 3600) return Math.floor(time2 / 60) + " minutes ago";
+//   if (time2 < 86400) return Math.floor(time2 / 3600) + " hours ago";
+//   return Math.floor(time2 / 86400) + " days ago";
+// }
+
+// console.log(timeAgo());
+
+
+// function countdown(){
+//     let newDate = new Date("2026-01-01 00:00:00")
+//     let currentDate = new Date();
+//     let difference = newDate - currentDate;
+//     if(difference <= 0){
+//         console.log("Time up");
+//     }
+//     let days = Math.floor(difference / (1000*60*60*24));
+//       let hours = Math.floor(difference / (1000*60*60) / 24);
+//   let mins = Math.floor(difference / (1000*60) / 60);
+//   let secs = Math.floor(difference / (1000 / 60));
+//   return days+" "+hours+" "+mins+" "+secs+" ";
+// }
+
+// console.log(countdown());
+
+// function difference() {
+//   let date1 = new Date("2025-09-15 08:00:00");
+//   let date2 = new Date("2025-09-15 11:25:00");
+//   let difference = Math.abs(date2 - date1);
+//   let hours = Math.floor(difference / (1000*60*60));
+//   let mins = Math.floor((difference / (1000*60)) % 60);
+//   return hours+" hours "+mins+" minutes";
+// }
+
+// console.log(difference());
+
+
+// function check() {
+//   let day = new Date().getDay();
+//   if(day == 0 || day == 6){
+//     return "Weekend"
+//   }
+//   else{
+//     return "Weekday";
+//   }
+// }
+
+// console.log(check());
+
+
+
