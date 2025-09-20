6c150e57d69c6bf6182a1b5d8bf47ac455aef8bf
Jawadbutt861 <jawadbutt861@gmail.com>
2025-09-21 00:14:24 +0500

    updated

---
+++ FOR IN & OF/script.js
@@ -0,0 +1,73 @@
+// For of loop
+
+// let numbers = [10, 20, 30, 40];
+
+// for(let i of numbers){
+//     console.log(i);
+// }
+
+// let names = ["Ali", "Jawad", "Sara", "Ayesha"];
+
+// for(let i of names){
+//     console.log(i);
+// }
+
+// let fruits = ["apple", "banana", "mango", "grapes"];
+
+// for(let i of fruits){
+//     console.log(i.toUpperCase());
+// }
+
+// let marks = [45, 67, 89, 32];
+// let sum = 0;
+// for(let i of marks){
+//     sum += i;
+// }
+
+// console.log(sum);
+
+// let word = "JavaScript";
+// let splitted = word.split("")
+// for(let i of splitted){
+//     console.log(i);   
+// }
+
+// let words = ["apple", "javascript", "loop", "practice"];
+// for(let i of words){
+//     console.log(i, i.length);
+// }
+
+
+
+
+// For in
+
+// let person = { name: "Ali", age: 21, city: "Lahore" };
+// for(let i in person){
+//     console.log(i,person[i]);
+//     console.log(person[i]);
+// }
+
+// let studentMarks = { Ali: 85, Sara: 90, Jawad: 78 };
+
+// for(let i in studentMarks){
+//     console.log(i, studentMarks[i]);
+// }
+
+
+// let car = { brand: "Toyota", model: "Corolla", year: 2020 };
+// let count = 0;
+// for(let i in car){
+//     count++;
+//     console.log(i, car[i]);
+    
+// }
+// console.log(count);
+
+// let colors = ["red", "green", "blue"];
+// for(let i in colors){
+//     console.log(i, colors[i]);
+// }
+
+
+
