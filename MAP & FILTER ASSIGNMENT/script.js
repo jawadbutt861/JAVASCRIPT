61e77ef167c61a7a430e6a832df6d956eff29544
jawadbutt <jawadbutt861@gmail.com>
2025-09-21 18:12:20 +0500

    Add files via upload

---
+++ MAP & FILTER ASSIGNMENT/script.js
@@ -0,0 +1,161 @@
+// let array = [1, 2, 3, 4, 5];
+// let doubled = array.map((item) =>{
+//     return item * 2
+// })
+
+// console.log(doubled);
+
+
+// let fruits = ["apple", "banana",
+// "cherry"];
+
+// let upperCase = fruits.map((item) =>{
+//     return item.toUpperCase();
+// })
+
+// console.log(upperCase);
+
+
+// let objs = [{name: "Alice"}, {name: "Bob"}, {name: "Charlie"}]
+
+// let names = objs.map((item) =>{
+//     return item.name;
+// })
+
+// console.log(names);
+
+
+// let animals = ["dog", "elephant",
+// "tiger"];
+
+// let length = animals.map((item) =>{
+//     return item.length
+// })
+// console.log(length);
+
+// let tasks = ["task1", "task2",
+// "task3"];
+
+// let done = tasks.map((item) =>{
+//     return `${item}-done`
+// })
+
+// console.log(done);
+
+// let nums = [2, 4, 6, 8]
+
+// let square = nums.map((item) =>{
+//     return item * item;
+// })
+// console.log(square);
+
+// let prices = [300,250,500,600,100,50]
+
+// let str = prices.map((item) =>{
+//     return `$${item}`
+// })
+// console.log(str);
+
+
+// let nums = [2,4,6,8,10,11,5,3,19]
+
+// let bool = nums.map((item) =>{
+//     if(item % 2 === 0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// })
+
+// console.log(bool);
+
+
+// let animals = ["dog", "elephant",
+// "tiger"];
+
+// let index = animals.map((item) =>{
+//     return `${item} , ${animals.indexOf(item)}`
+// })
+
+// console.log(index);
+
+// let nums = [1, 2, 3, 4, 5, 6]
+
+// let evens = nums.filter((item) =>{
+//     return item % 2 === 0;
+// })
+// console.log(evens);
+
+// let negative = [-3, 4, -1, 7, -5]
+// let filtered = negative.filter((item) =>{
+//     return item > 0
+// })
+// console.log(filtered);
+
+// let names = ["Joe", "Elizabeth",
+// "Tom", "Anna"]
+
+// let length = names.filter((item) =>{
+//     return item.length > 4
+// })
+// console.log(length);
+
+// let users = [{name: "Alice", active: true}, {name: "Bob", active: false}];
+
+// let active = users.filter((item) =>{
+//     return item.active === true
+// })
+// console.log(active);
+
+
+// let words = ["apple", "river", "mountain", "sky", "computer", "tree", "house", "light", "dream", "book"]
+
+// let filtered = words.filter((item) =>{
+//     if(item.charAt(0) === 'a'){
+//         return item
+//     }
+// })
+
+// console.log(filtered);
+
+// let nums = [5, 12, 7, 34, 18, 23, 1, 45, 9, 30, 17, 26, 3, 8, 14, 21, 39, 6, 10, 27]
+
+// let filtered = nums.filter((item) =>{
+//     return item > 10
+// })
+
+// console.log(filtered);
+
+// let code = ["decode", "apple", "codebase", "mountain", "encoding", "river", "codename", "sky", "recoded", "forest"]
+
+// let coded = code.filter((item) =>{
+//     return item.includes('code')
+// })
+
+// console.log(coded);
+
+//  let mixedArray = [null, undefined, 0, "", false, 42, "hello", true, [], {}, "code"];
+
+//  let filtered = mixedArray.filter((item) =>{
+//     return !(item == null || item == undefined)
+//  })
+
+//  console.log(filtered);
+ 
+
+// let arr = [{name: "Phone", price: 99}, {name: "Tablet", price: 120}];
+
+// let price = arr.filter((item) =>{
+//     return item.price < 100
+// })
+// console.log(price);
+
+// let sum = [1, 2, 3, 4]
+
+// let add = sum.reduce((acc,cval) =>{
+//     return acc + cval;
+// },0)
+// console.log(add);
+
+
